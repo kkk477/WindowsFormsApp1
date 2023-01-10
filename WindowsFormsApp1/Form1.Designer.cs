0bf1421025c254e48539f0c5bdfe34f470db31d3
HoWoong <dlghdnd@gmail.com>
2023-01-10 23:29:54 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IDPath = new System.Windows.Forms.TextBox();
+            this.PWPath = new System.Windows.Forms.TextBox();
+            this.btnConnTest = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.settime = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Tray_Icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Context_TrayIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Context_TrayIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PW";
+            // 
+            // IDPath
+            // 
+            this.IDPath.Location = new System.Drawing.Point(55, 26);
+            this.IDPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.IDPath.Name = "IDPath";
+            this.IDPath.Size = new System.Drawing.Size(182, 21);
+            this.IDPath.TabIndex = 4;
+            // 
+            // PWPath
+            // 
+            this.PWPath.Location = new System.Drawing.Point(310, 26);
+            this.PWPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PWPath.Name = "PWPath";
+            this.PWPath.PasswordChar = '*';
+            this.PWPath.Size = new System.Drawing.Size(182, 21);
+            this.PWPath.TabIndex = 5;
+            // 
+            // btnConnTest
+            // 
+            this.btnConnTest.Location = new System.Drawing.Point(526, 27);
+            this.btnConnTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConnTest.Name = "btnConnTest";
+            this.btnConnTest.Size = new System.Drawing.Size(91, 20);
+            this.btnConnTest.TabIndex = 6;
+            this.btnConnTest.Text = "연결 테스트";
+            this.btnConnTest.UseVisualStyleBackColor = true;
+            this.btnConnTest.Click += new System.EventHandler(this.btnConnTest_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(526, 64);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 20);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // settime
+            // 
+            this.settime.CustomFormat = "hh:mm tt";
+            this.settime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.settime.Location = new System.Drawing.Point(55, 61);
+            this.settime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.settime.Name = "settime";
+            this.settime.Size = new System.Drawing.Size(106, 21);
+            this.settime.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(369, 66);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "비밀번호 보이기";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(194, 66);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 16);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "퇴근 찍고 PC 종료";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Tray_Icon
+            // 
+            this.Tray_Icon.Icon = ((System.Drawing.Icon)(resources.GetObject("Tray_Icon.Icon")));
+            this.Tray_Icon.Text = "FG204 Second EDITION Ver.2.31";
+            this.Tray_Icon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Tray_Icon_MouseDoubleClick);
+            // 
+            // Context_TrayIcon
+            // 
+            this.Context_TrayIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.Context_TrayIcon.Name = "Context_TrayIcon";
+            this.Context_TrayIcon.Size = new System.Drawing.Size(105, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 110);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.settime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnConnTest);
+            this.Controls.Add(this.PWPath);
+            this.Controls.Add(this.IDPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FG204 Second EDITION Ver.2.31";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.Context_TrayIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IDPath;
+        private System.Windows.Forms.TextBox PWPath;
+        private System.Windows.Forms.Button btnConnTest;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DateTimePicker settime;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NotifyIcon Tray_Icon;
+        private System.Windows.Forms.ContextMenuStrip Context_TrayIcon;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,304 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Selenium
+        private ChromeDriverService _driverService = null;
+        private ChromeOptions _options = null;
+        private ChromeDriver _driver = null;
+
+        // Login info
+        string _url = "http://pms.ictway.co.kr/Login";
+        //string _url = "";
+        string _id = String.Empty;
+        string _pw = String.Empty;
+        DateTime _sendTime;
+        bool _checkShutdown = false;
+
+        CancellationToken token;
+        CancellationTokenSource tokenSource;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += TrayIcon_Load;
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("--disable-gpu");
+            _options.AddArgument("--headless"); // 크롬창 숨김
+
+            tokenSource = new CancellationTokenSource();
+            token = tokenSource.Token;
+        }
+        //http://pms.ictway.co.kr/Login
+        ////*[@id="form1"]/div[3]/div[1]/div[3]/ul[1]/li[2]/div/span - 퇴근버튼
+
+        /// <summary>
+        /// 접속 테스트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnTest_Click(object sender, EventArgs e)
+        {
+            _id = IDPath.Text;
+            _pw = PWPath.Text;
+
+            try
+            {
+                _driver = new ChromeDriver(_driverService, _options);
+                _driver.Navigate().GoToUrl(_url);
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                var elementID = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"txtID\"]"));    // ID
+                elementID.SendKeys(_id);
+
+                var elementPW = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"txtPW\"]"));    // PW
+                elementPW.SendKeys(_pw);
+
+                var elementLG = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"form1\"]/div[3]/div/div/div[3]"));    // Login
+                elementLG.Click();
+
+                Task.Delay(3000);
+
+                var elementOut = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"form1\"]/div[3]/div[1]/div[3]/ul[1]/li[2]/div/span"));    // 퇴근버튼
+                if (elementOut != null)
+                    MessageBox.Show("로그인 성공!!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                _driver.Close();
+            }
+        }
+
+        /// <summary>
+        /// 퇴근 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.Compare(btnStart.Text, "중지") == 0)
+            {
+                btnStart.Text = "시작";
+                btnStart.Refresh();
+                tokenSource.Cancel();
+                return;
+            }
+
+            if (string.Compare(btnStart.Text, "시작") == 0)
+            {
+                tokenSource = new CancellationTokenSource();
+                token = tokenSource.Token;
+                btnStart.Text = "중지";
+                btnStart.Refresh();
+            }
+
+            if (string.IsNullOrEmpty(IDPath.Text))
+                return;
+
+            if (string.IsNullOrEmpty(PWPath.Text))
+                return;
+
+            _id = IDPath.Text;
+            _pw = PWPath.Text;
+            _sendTime = settime.Value;
+            _checkShutdown = checkBox2.Checked;
+            TimeSpan delayTime = _sendTime - DateTime.Now;
+            if (delayTime.Milliseconds <= 0)
+                return;
+
+            try
+            {
+                await Task.Delay(delayTime, token);
+
+                if (token.IsCancellationRequested)
+                    return;
+
+                //await Task.Run(() => { test(_id, _pw, _checkShutdown); }, token);
+                await Task.Run(() => { LeaveWork(_id, _pw, _checkShutdown); }, token);
+                btnStart.Text = "시작";
+                btnStart.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void test(string id, string pw, bool checkShutdown)
+        {
+            MessageBox.Show("Hello world!!");
+        }
+        /// <summary>
+        /// 퇴근 기능
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="pw"></param>
+        /// <param name="checkShutdown"></param>
+        private async void LeaveWork(string id, string pw, bool checkShutdown)
+        {
+            string __id = id;
+            string __pw = pw;
+            bool __checkShutdown = checkShutdown;
+
+            try
+            {
+                _driver = new ChromeDriver(_driverService, _options);
+                _driver.Navigate().GoToUrl(_url);
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                var elementID = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"txtID\"]"));    // ID
+                elementID.SendKeys(__id);
+
+                var elementPW = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"txtPW\"]"));    // PW
+                elementPW.SendKeys(__pw);
+
+                var elementLG = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"form1\"]/div[3]/div/div/div[3]"));    // Login
+                elementLG.Click();
+
+                await Task.Delay(3000);
+
+                var elementOut = _driver.FindElement(OpenQA.Selenium.By.XPath("//*[@id=\"form1\"]/div[3]/div[1]/div[3]/ul[1]/li[2]/div/span"));    // 퇴근버튼
+                elementOut.Click();
+
+                await Task.Delay(500);
+
+                var alert = _driver.SwitchTo().Alert();
+                alert.Accept();
+                
+                await Task.Delay(500);
+
+                if (__checkShutdown)
+                    ShutdownComputer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                _driver.Close();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //if(_driver != null)
+            //    _driver.Close();
+        }
+
+        private void TrayIcon_Load(object sender, EventArgs e)
+        {
+            Tray_Icon.ContextMenuStrip = Context_TrayIcon;
+        }
+
+        /// <summary>
+        /// 트레이 아이콘 더블클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Tray_Icon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        /// <summary>
+        /// 트레이 Show 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        /// <summary>
+        /// 트레이 Exit 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 최소화 액션
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                Tray_Icon.Visible = true;
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                Tray_Icon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        /// <summary>
+        /// 비밀번호 보이기 액션
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                PWPath.PasswordChar = Char.MinValue;
+                PWPath.Refresh();
+            }
+            else if (checkBox1.Checked == false)
+            {
+                PWPath.PasswordChar = '*';
+                PWPath.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// 사용자 PC 종료
+        /// </summary>
+        private void ShutdownComputer()
+        {
+            Process proc = new Process();
+            Process.Start("cmd.exe", "Shutdown -s -t 0 0");
+        }
+
+    }
+}
